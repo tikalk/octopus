7b5144073bee8d85dcd670b163d55f92a317005e
ShavitCohen <shavit.hit@gmail.com>
2018-12-05 16:18:39 +0200

    :wrench: (backend) - adding production port for cross origin

--- backend/index.js
+++ backend/index.js
@@ -4,30 +4,30 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const api = require('./api');
 const _ = require('lodash');
-require('dotenv').config()
+require('dotenv').config();
 
 const PORT = process.env.PORT || 3333;
-const whitelist = ['https://localhost:8080', 'http://localhost:8080'];
-const {tokenToUserMW} = require('./middlewares/token.middleware');
+const whitelist = ['http://localhost:8080', 'http://octopus.tikal.io'];
+const { tokenToUserMW } = require('./middlewares/token.middleware');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: (origin, cb) => {
-        if (!origin || _.includes(whitelist, origin)) {
-            return cb(null, true);
-        }
-        return cb(new Error('Not allowed by CORS'));
-    },
+  origin: (origin, cb) => {
+    if (!origin || _.includes(whitelist, origin)) {
+      return cb(null, true);
+    }
+    return cb(new Error('Not allowed by CORS'));
+  },
 }));
 
 app.use('/api', tokenToUserMW, api);
 
 process.on('unhandledRejection', (reason, p) => {
-    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+  console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running at http://localhost:${PORT}`);
 });
