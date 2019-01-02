b53bad4940a470b71716f16c88b8654b4126d238
ShavitCohen <shavit.hit@gmail.com>
2019-01-02 20:22:12 +0200

    :sparkles: (server) - handling roles and adding roadmap to conf file

--- backend/api/topics/topics.controller.js
+++ backend/api/topics/topics.controller.js
@@ -1,53 +1,58 @@
 const _ = require('lodash');
-const {authorization} = require('./../../utils/google-authorization');
-const {getSheetData, formatData} = require('../../utils/spreadshit-data-fetcher');
-const {topics} = require('../../dataConfig');
+const { authorization } = require('./../../utils/google-authorization');
+const { getSheetData, formatData } = require('../../utils/spreadshit-data-fetcher');
+const { topics } = require('../../dataConfig');
 
 const getTopics = async (req, res) => {
-    const {group: userGroup, role: userRole} = req.auth;
-    const topicsToReturn = _.keys(topics).reduce((acc, topicId) => {
-        const topic = _.get(topics, topicId);
-        const {title} = topic;
-        if (topic.roles && !topic.roles.includes(userRole)) {
-            return;
-        }
-        if (topic.group && !topic.groups.includes(userGroup)) {
-            return;
-        }
-        acc.push({id: topicId, title});
-        return acc;
-    }, []);
+  const { group: userGroup, role: userRole } = req.auth;
+  const topicsToReturn = _.keys(topics).reduce((acc, topicId) => {
+    const topic = _.get(topics, topicId);
+    const { title } = topic;
+    if (topic.roles && !topic.roles.includes(userRole)) {
+      return;
+    }
+    if (topic.group && !topic.groups.includes(userGroup)) {
+      return;
+    }
+    acc.push({ id: topicId, title });
+    return acc;
+  }, []);
 
-    res.json(topicsToReturn);
+  res.json(topicsToReturn);
 };
 
 const getTopicData = async (req, res) => {
-    const {group: userGroup, role: userRole} = req.auth;
-    const {identifiers} = req.query;
-    const {topicId} = req.params;
+  const { group: userGroup, role: userRole } = req.auth;
+  const { identifiers } = req.query;
+  const { topicId } = req.params;
 
-    try {
-        const auth = await authorization();
-        const topic = topics[topicId];
-        const sheetData = await getSheetData({topic, auth, identifiers});
-        const data = formatData({topic, sheetData, userGroup, userRole});
+  try {
+    const auth = await authorization();
+    const topic = topics[topicId];
+    const sheetData = await getSheetData({ topic, auth, identifiers });
+    const data = formatData({ topic, sheetData, userGroup, userRole });
 
-        if (topic.extend) {
-            for (const eTopic of topic.extend) {
-                const extendSheetData = await getSheetData({topic: eTopic, auth, identifiers});
-                const extendData = formatData({topic: eTopic, sheetData: extendSheetData, userGroup, userRole});
-                data.push(_.first(extendData))
-            }
-        }
-
-        res.json(data);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
+    if (topic.extend) {
+      for (const eTopic of topic.extend) {
+        const extendSheetData = await getSheetData({ topic: eTopic, auth, identifiers });
+        const extendData = formatData({
+          topic: eTopic,
+          sheetData: extendSheetData,
+          userGroup,
+          userRole,
+        });
+        data.push(_.first(extendData));
+      }
     }
+
+    res.json(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
 
 module.exports = {
-    getTopics,
-    getTopicData,
+  getTopics,
+  getTopicData,
 };
--- backend/utils/spreadshit-data-fetcher.js
+++ backend/utils/spreadshit-data-fetcher.js
@@ -1,54 +1,59 @@
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 const sheets = google.sheets('v4');
+const { ALL } = require('../constants');
 
-const _filterDataByEmployee = ({topic, sheetData, identifiers}) =>
-    sheetData.filter(row =>
-        identifiers.find(identifier => identifier === row[topic.employeeIdentifierIndex]));
+const _filterDataByEmployee = ({ topic, sheetData, identifiers }) =>
+  sheetData.filter(row =>
+    identifiers.find(identifier => identifier === row[topic.employeeIdentifierIndex]));
 
-const getSheetData = async ({topic, auth, identifiers}) => {
-    const {spreadsheetId, sheetId, range} = topic;
-    const request = {
-        // The ID of the spreadsheet to retrieve data from.
-        spreadsheetId,  // TODO: Update placeholder value.
-        range: `'${sheetId}'!${range}`,  // TODO: Update placeholder value.
-        majorDimension: 'ROWS',
-        valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
-        dateTimeRenderOption: 'FORMATTED_STRING',  // TODO: Update placeholder value.
-        auth: auth,
-    };
+const getSheetData = async ({ topic, auth, identifiers }) => {
+  const { spreadsheetId, sheetId, range } = topic;
+  const request = {
+    // The ID of the spreadsheet to retrieve data from.
+    spreadsheetId,  // TODO: Update placeholder value.
+    range: `'${sheetId}'!${range}`,  // TODO: Update placeholder value.
+    majorDimension: 'ROWS',
+    valueRenderOption: 'FORMATTED_VALUE',  // TODO: Update placeholder value.
+    dateTimeRenderOption: 'FORMATTED_STRING',  // TODO: Update placeholder value.
+    auth: auth,
+  };
 
-    return new Promise((resolve, reject) => {
-        sheets.spreadsheets.values.get(request, (err, response) => {
-            if (err) {
-                return reject(err);
-            }
-            const {data: {values}} = response;
-            return resolve(identifiers ? _filterDataByEmployee({topic, sheetData: values, identifiers}) : values)
-        });
+  return new Promise((resolve, reject) => {
+    sheets.spreadsheets.values.get(request, (err, response) => {
+      if (err) {
+        return reject(err);
+      }
+      const { data: { values } } = response;
+      return resolve(identifiers ? _filterDataByEmployee({
+        topic,
+        sheetData: values,
+        identifiers,
+      }) : values);
     });
+  });
 };
 
-const formatData = ({sheetData, topic, userGroup, userRole}) => {
-    const {fields, sectionTitle} = topic;
-    return sheetData.map(row => {
-        const formattedFields = fields.reduce((acc, field) => {
-            if (field.roles && !field.roles.includes(userRole)) return;
-            if (field.group && !field.group.includes(userGroup)) return;
+const formatData = ({ sheetData, topic, userGroup, userRole }) => {
+  const { fields, sectionTitle } = topic;
+  return sheetData.map(row => {
+    const formattedFields = fields.reduce((acc, field) => {
+      if (field.roles && !field.roles.includes(userRole)) return;
+      if (userGroup !== ALL && field.group && !field.group.includes(userGroup)) return;
 
-            const {title, grid, index} = field;
-            acc.push({title, grid, value: row[index] || 'N/A', index});
-            return acc;
-        }, []);
+      const { title, grid, index } = field;
+      acc.push({ title, grid, value: row[index] || 'N/A', index });
+      return acc;
+    }, []);
 
-        return {
-            sectionTitle: row[sectionTitle.index],
-            fields: formattedFields,
-        };
-    });
+    return {
+      sectionTitle: row[sectionTitle.index],
+      fields: formattedFields,
+    };
+  });
 
 };
 
 module.exports = {
-    getSheetData,
-    formatData,
+  getSheetData,
+  formatData,
 };
