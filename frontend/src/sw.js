551a1d9a9c829262a39aafb17117375f9e61c2e9
ShavitCohen <shavit.hit@gmail.com>
2019-10-02 12:16:13 +0300

    :recycle: (frontend) - adding api to service worker from offline

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,7 +1,7 @@
 importScripts('https://www.gstatic.com/firebasejs/6.0.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/6.0.1/firebase-messaging.js');
 
-workbox.setConfig({debug: true}); //eslint-disable-line
+workbox.setConfig({ debug: true }); //eslint-disable-line
 
 /**
  * Precache handling
@@ -9,18 +9,14 @@ workbox.setConfig({debug: true}); //eslint-disable-line
 const initCaching = () => {
   //precache from precache manifest
   workbox.precaching.precacheAndRoute(self.__precacheManifest || []); //eslint-disable-line
-  const {CacheFirst} = workbox.strategies;
+  const { CacheFirst, NetworkFirst } = workbox.strategies;
   //images
-  workbox.routing.registerRoute(
-    /(.*)\.(?:png|gif|jpg|svg|ico|jpeg)/,
-    new CacheFirst({cacheName: 'images'})
-  );
+  workbox.routing.registerRoute(/(.*)\.(?:png|gif|jpg|svg|ico|jpeg)/, new CacheFirst({ cacheName: 'images' }));
 
   //fonts
-  workbox.routing.registerRoute(
-    /(.*)\.(?:ttf|woff)/,
-    new CacheFirst({cacheName: 'fonts'})
-  );
+  workbox.routing.registerRoute(/(.*)\.(?:ttf|woff)/, new CacheFirst({ cacheName: 'fonts' }));
+  //api
+  workbox.routing.registerRoute(/^https:\/\/octopus-be.tikal.io\/api\//, new NetworkFirst({ cacheName: 'api' }));
 };
 
 initCaching();
