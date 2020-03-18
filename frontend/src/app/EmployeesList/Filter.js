620c074d1af07e5e7d0f3fb031a6940ab544f012
ShavitCohen <shavit.hit@gmail.com>
2020-03-18 16:15:42 +0200

    :recycle: (frontend) fixing filter style

--- frontend/src/app/EmployeesList/Filter.js
+++ frontend/src/app/EmployeesList/Filter.js
@@ -15,20 +15,20 @@ const Filter = ({ obj, onFilterChange }) => {
 
   return (
     <Grid container spacing={3}>
-      <Grid item xs={12} container spacing={2}>
-        <Grid item xs={3}>
+      <Grid xs={12} container spacing={2}>
+        <Grid item xs={3} style={{ paddingRight: 16 }}>
           <IconButton color="primary" aria-label="show hide filter" component="span" onClick={handleFilterButtonClick}>
             <FilterList />
           </IconButton>
         </Grid>
         <Grid item xs={9}>
-          <Typography noWrap={true} style={{ marginTop: 10 }}>
+          <Typography noWrap={true} style={{ marginTop: 18, fontSize:10 }}>
             {selectedFilterItems.join(', ')}
           </Typography>
         </Grid>
       </Grid>
       {showItems && (
-        <Grid item xs={12}>
+        <Grid item xs={12} style={{ paddingRight: 20 }}>
           <FormGroup>
             {Object.keys(obj).map(name => {
               const value = obj[name];
